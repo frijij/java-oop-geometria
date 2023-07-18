c65cbbd415f9c3bd32e87ea08b39474e4da0a39a
francesca <francipeco36@gmail.com>
2023-07-18 14:56:02 +0200

    file preparation

---
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,18 @@
+package org.lessons.java.geometria;
+
+/*
+ inserire le classi:
+una classe Rettangolo con due attributi interi: base e altezza. Aggiungere un opportuno costruttore
+con parametri. Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono,
+rispettivamente, l’area e il perimetro del rettangolo.
+ */
+public class Rettangolo {
+
+    //1. campi: interi base e altezza
+
+
+    //2. costruttori
+
+
+    //3. metodi
+}
